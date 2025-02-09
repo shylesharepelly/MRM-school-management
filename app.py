1cff268412f0513fb9c50d65fae0888fba626c56
Shylesh <shylesharepelly@gmail.com>
2025-02-09 14:53:03 +0530

    added mobile view

--- app.py
+++ app.py
@@ -1711,7 +1711,7 @@ def add_subject():
 
 
 if __name__ == '__main__':
-    # app.run(debug=False)
+    # app.run(debug=True)
     # app.run()
     port = int(os.environ.get("PORT", 5000))  # Use Render's $PORT or default to 5000
     app.run(host="0.0.0.0", port=port)
